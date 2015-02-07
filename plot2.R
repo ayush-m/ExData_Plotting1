ca9afe777d1ad74b9443d675107abbc61c991a6a
Ayush Mananiya <ayushmananiya007@gmail.com>
2015-02-07 19:22:16 +0530

    Update plot2.R

--- plot2.R
+++ plot2.R
@@ -8,7 +8,7 @@ data<-read.table("household_power_consumption.txt", header=TRUE, sep=";", na.str
 data$Date<-as.Date(data$Date, format="%d/%m/%Y")  #setting the Date column of data as Date datatype
 data<-data[data$Date<="2007-02-02" & data$Date>="2007-02-01",]  #Extract those rows whose dates in Date column are between 01/feb/2007 and 02/feb/2007
 data$Date<-as.POSIXct(paste(data$Date, data$Time), format="%Y-%m-%d %H:%M:%S")  #Combine Date and Time column into one column which represents date and time both in foramt "Year-Month-Dte Hour:Minute:Seconds"
-png(file="plot2.png") #for opening PNG file device as the name plot1.png
+png(file="plot2.png") #for opening PNG file device as the name plot2.png
 par(bg=NA)  #Set the background parameter as transparent
 plot(data$Date,data$Global_active_power,type="l", xlab="" , ylab="Global Active Power (kilowatts)") #plot a line graph of Date(which now includes data and time both) vs. Global_active_power
 dev.off() #close all the graphic devices
