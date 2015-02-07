eb56c66f22cfee178c8eb92a1c9bcdc973246249
Ayush Mananiya <ayushmananiya007@gmail.com>
2015-02-07 16:34:24 +0530

    Update plot2.R

--- plot2.R
+++ plot2.R
@@ -1,3 +1,13 @@
+#The given txt file contains the data for various dates.Firstly we will start by reading the data from file into
+#object named as "data".
+#Also we need to subset the date according to our requirement.
+#Here, as the requirement I have took the dates between 01/feb/2007 and 02/feb/2007
+#Now we will be ploting the graph of time vs 
+# we have to plot a histogram of that column.
+
+
+
+
 data<-read.table("household_power_consumption.txt", header=TRUE, sep=";", na.strings="?", nrows=69990)
 data$Date<-as.Date(data$Date, format="%d/%m/%Y")
 data<-data[data$Date<="2007-02-02" & data$Date>="2007-02-01",]
